edc0e5a4ad689364805fb3bd26c5664f5337a181
Antônio Rocha <antoniojunior159@gmail.com>
2022-07-20 09:23:49 -0300

    update

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -1,6 +1,8 @@
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using PokemonReviewApp.Interfaces;
+using PokemonReviewApp.Models;
+using PokemonReviewApp.Dto;
 
 namespace PokemonReviewApp.Controllers
 {
@@ -18,18 +20,52 @@ namespace PokemonReviewApp.Controllers
             _mapper = mapper;
         }
 
+        //Retornar todas as categorias
         [HttpGet]
-        [ProducesResponseType(200, Type = typeof(IEnumerable<Pokemon>))] //Opcional
-        
-        //Aqui está retornando uma lista
-        public IActionResult GetCategories() //Do Repository/PokemonRepository.cs
+        [ProducesResponseType(200, Type = typeof(IEnumerable<Category>))] //Opcional
+       
+        public IActionResult GetCategories() 
         {
-            var pokemons = _mapper.Map<List<PokemonDto>>(_pokemonRepository.GetPokemons());
+            var categories = _mapper.Map<List<CategoryDto>>(_categoryRepository.GetCategories());
 
             if(!ModelState.IsValid)
             {
                 return BadRequest(ModelState);
             }
+            return Ok(categories);
+        }
+
+        //Pegar a Category pelo Id        
+        [HttpGet("{categoryId}")]
+        [ProducesResponseType(200, Type = typeof(Category))] 
+        [ProducesResponseType(400)] 
+
+        public IActionResult GetCategory(int categoryId) 
+        {
+            if (!_categoryRepository.CategoryExists(categoryId))
+                return NotFound();
+
+            var category = _mapper.Map<CategoryDto>(_categoryRepository.GetCategory(categoryId));
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            return Ok(category);
+        }
+
+        //?
+        [HttpGet("pokemon/{categoryId}")]
+        [ProducesResponseType(200, Type = typeof(IEnumerable<Pokemon>))] 
+        [ProducesResponseType(400)] 
+
+        public IActionResult GetPokemonByCategoryId(int categoryId)
+        {
+            var pokemons = _mapper.Map<List<PokemonDto>>(
+                _categoryRepository.GetPokemonByCategory(categoryId));
+
+            if (!ModelState.IsValid)
+                return BadRequest();
+
             return Ok(pokemons);
         }
     }
--- Program.cs
+++ Program.cs
@@ -17,6 +17,8 @@ builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 //Adicionado após o RP e DI-----------------------------------------------
 builder.Services.AddScoped<IPokemonRepository, PokemonRepository>();
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+builder.Services.AddScoped<ICountryRepository, CountryRepository>();
 //---------------------------------------
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
