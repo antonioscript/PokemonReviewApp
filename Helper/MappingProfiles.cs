cf39e1c711a26fcac451c8d9735f0a576709f538
Antônio Rocha <antoniojunior159@gmail.com>
2022-07-20 16:38:39 -0300

    update

--- Helper/MappingProfiles.cs
+++ Helper/MappingProfiles.cs
@@ -11,9 +11,9 @@ namespace PokemonReviewApp.Helper
             CreateMap<Pokemon, PokemonDto>();
             CreateMap<Category, CategoryDto>();
             CreateMap<Country, CountryDto>();
-            CreateMap<OwnerDto, Owner>();
-            CreateMap<ReviewDto, Review>();
-            CreateMap<ReviewerDto, Reviewer>();
+            CreateMap<Owner, OwnerDto>();
+            CreateMap<Review, ReviewDto>();
+            CreateMap<Reviewer, ReviewerDto>();
         }
     }
 }
